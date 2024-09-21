bd7378c3d6fbfc24a6a57da3bfedca0377e6d06c
Sonpr0x <82546097+Sonpr0x@users.noreply.github.com>
2024-09-21 07:48:44 +0700

    Delete cloth_app/config.py

--- cloth_app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'mysql://cloth_project:trycatch@localhost/myappdb'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
