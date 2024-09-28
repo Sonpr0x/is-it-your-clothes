4a8f99867c8a237801409e91b118740004362949
Sonpr0x <hoangdvjs2008019@fpt.edu.vn>
2024-09-28 11:53:34 +0700

    idk

--- cloth_app/routes.py
+++ cloth_app/routes.py
@@ -1,8 +1,7 @@
 from flask import render_template, redirect, url_for, request, flash, send_file, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db, login_manager
-from models import User, Image
+from app import app, db, login_manager, client
 import os, uuid
 
 
