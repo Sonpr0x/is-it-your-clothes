7e903acd031a86c9bc6c689e85527f80f967a9ea
Sonpr0x <hoangdvjs2008019@fpt.edu.vn>
2024-09-30 23:43:05 +0700

    login - logout UI

--- cloth_app/models.py
+++ cloth_app/models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 class User(UserMixin, db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
     images = db.relationship('Image', backref='user', lazy=True)
 
 class Image(db.Model):
