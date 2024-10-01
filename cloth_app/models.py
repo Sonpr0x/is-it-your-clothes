b989fa0bb880f000af7667e6285858cce023c8b0
Sonpr0x <hoangdvjs2008019@fpt.edu.vn>
2024-10-01 21:59:16 +0700

    update

--- cloth_app/models.py
+++ cloth_app/models.py
@@ -6,6 +6,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(300), nullable=False)
     images = db.relationship('Image', backref='user', lazy=True)
+    admin = db.Column(db.Boolean, nullable=False)
 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
